eb8b715f1a64768eb62f1be656308bd0ef7a60e6
zhouzhou <2821511460@qq.com>
2024-03-11 18:36:03 +0800

    300

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(a : i32) -> i32 {
+    
+    if  a <= 40{
 
+     2*a
+    }
+    else{
+        a
+    }
+ 
+ }
 // Don't modify this function!
 #[test]
 fn verify_test() {
