92b6bff7e9b524a46a9ece979cee9837e7a3ef32
zhouzhou <2821511460@qq.com>
2024-03-13 09:45:10 +0800

    100

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,16 +19,29 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
-
+ 
+// enum Syh {
+//     EmptyInput,
+//     InvalidInput,
+// }
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-
+    let qty = item_quantity.parse::<i32>()?;
     Ok(qty * cost_per_item + processing_fee)
+    // if item_quantity.is_empty(){
+    //     return Err(Syh::EmptyInput);
+    // }
+    // let qty = match item_quantity.parse::<i32>(){
+    //     Ok(qty) => qty,
+    //     Err(_) > return Err(Syh::InvalidInput),
+    // };
+    // Ok(qty * cost_per_item + processing_fee)
+
+
+  
 }
 
 #[cfg(test)]
